906e085b95191d70ba2e126c871cab4bd5f35984
Your Name <you@example.com>
2019-04-28 11:28:17 -0400

    First refactoring of the test cases

--- src/test/groovy/com/harriskirk/jobhunt/broad/mbta/MbtaReporter.groovy
+++ src/test/groovy/com/harriskirk/jobhunt/broad/mbta/MbtaReporter.groovy
@@ -6,15 +6,19 @@ public class MbtaReporterTest extends GroovyTestCase {
 
   void testStopsWithTwoOrMoreRoutes() {
     
-    def masterRouteList = [
-      new Route(id:'R', stops:['A', 'P', 'Q']),
-      new Route(id:'O', stops:['C', 'H', 'O']),
-      new Route(id:'G', stops:['L', 'P', 'Y']),
-      new Route(id:'B', stops:['X', 'U', 'M', 'S']),
+    def testCase1 = [
+      routeList: [
+        new Route(id:'R', stops:['A', 'P', 'Q']),
+        new Route(id:'O', stops:['C', 'H', 'O']),
+        new Route(id:'G', stops:['L', 'P', 'Y']),
+        new Route(id:'B', stops:['X', 'U', 'M', 'S']),
+      ],
+      expectedStops: [P: ['G', 'R'].toSet() ]
     ]
 
-    Map expectedStopsWithMultiRoutes = [P: ['G', 'R'].toSet() ]
-    def actualStopsWithMultiRoutes = MbtaReporter.getStopsWithMultiRoutes ( masterRouteList )
+    // Map expectedStopsWithMultiRoutes = [P: ['G', 'R'].toSet() ]
+    Map expectedStopsWithMultiRoutes = testCase1.expectedStops
+    def actualStopsWithMultiRoutes = MbtaReporter.getStopsWithMultiRoutes ( testCase1.routeList )
 
     assertEquals ( expectedStopsWithMultiRoutes.keySet(),  actualStopsWithMultiRoutes.keySet() )
     assertEquals ( expectedStopsWithMultiRoutes.get('P').toSet().toString(), actualStopsWithMultiRoutes.get('P').toSet().toString() )
